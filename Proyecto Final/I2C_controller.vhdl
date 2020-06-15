1fd9b28b90c0151389f53132dc1afdbf8414349c
rogeliozazueta <61670163+rogeliozazueta@users.noreply.github.com>
2020-06-15 00:04:04 -0500

    Update I2C_controller.vhdl
    Cambios v.2.0

--- Proyecto Final/I2C_controller.vhdl
+++ Proyecto Final/I2C_controller.vhdl
@@ -1,4 +1,4 @@
-library ieee;
+llibrary ieee;
 use ieee.std_logic_1164.all;
 
 entity I2C_controller is
@@ -6,14 +6,14 @@ entity I2C_controller is
         SlaveAddress : std_logic_vector(6 downto 0) := "1101000"
     );
     port (
-        clk     : in    std_logic;
-        reset   : in    std_logic;
-        SCL     : out   std_logic;
-        SDA     : inout std_logic;
-        RW      : in    std_logic;
-        DataIn  : in    std_logic_vector(7 downto 0);
-        Ack     : out   std_logic;
-        DataOut : out   std_logic_vector(7 downto 0)
+        CLK_50_MHz : in    std_logic;
+        reset      : in    std_logic;
+        SCL        : out   std_logic;
+        SDA        : inout std_logic;
+        RW         : in    std_logic;
+        DataIn     : in    std_logic_vector(7 downto 0);
+        Ack        : out   std_logic;
+        DataOut    : out   std_logic_vector(7 downto 0)
     );
 
 end I2C_controller;
@@ -25,27 +25,37 @@ architecture arch OF I2C_controller is
             SLV_ACK1, SLV_ACK2,
             WRDATA1,WRDATA2,
             SLV_ACK3,SLV_ACK4,
-            STOP1,STOP2,STOP3
+            STOP1,STOP2,STOP3,
+            RDDATA1,RDDATA2,
+            NACK1,NACK2,
+            STOP4,STOP5,STOP6,
+            ACK_ERROR
         );
 
     signal state           : statetype;
+    SIGNAL Data            : std_logic_vector(7 downto 0);
     SIGNAL SDA01           : std_logic;
     SIGNAL bitcount        : integer RANGE 0 TO 7;
     signal SlaveAddress_RW : std_logic_vector(7 downto 0);
+
+    constant C250          : integer := 250;
+    signal ClkCountDivider : integer RANGE 0 TO C250;
+    signal CLK_200k_Hz     : std_logic;
 
 begin
 
     SlaveAddress_RW <= SlaveAddress&RW;
     SDA             <= 'Z' when SDA01 = '1' else '0';
 
-    output : process(clk, reset)
+    output : process(CLK_200k_Hz, reset)
     begin
         if(reset = '1') then
-            SCL   <= '1';
-            SDA01 <= '1';
-            Ack   <= '0';
-            state <= IDLE;
-        elsif(clk'EVENT and clk = '1') then
+            SCL     <= '1';
+            SDA01   <= '1';
+            Ack     <= '0';
+            DataOut <= "00000000";
+            state   <= IDLE;
+        elsif(CLK_200k_Hz'EVENT and CLK_200k_Hz = '1') then
             case state is
                 -- En IDLE SDA y SCL = 1
                 when IDLE =>
@@ -79,10 +89,16 @@ begin
                     SDA01 <= '1';
                     state <= SLV_ACK2;
                 when SLV_ACK2 =>
-                    SCL   <= '1';
-                    Ack   <= SDA;
-                    state <= WRDATA1; -- Escritura
-                                      -- Enviar datos al esclavo
+                    SCL <= '1';
+                    Ack <= SDA;
+                    if SDA = '1' then
+                        state <= ACK_ERROR;
+                    elsif RW = '0' then
+                        state <= WRDATA1; -- Escritura
+                    else
+                        state <= RDDATA1; -- Lectura
+                    end if;
+                -- Enviar datos al esclavo
                 when WRDATA1 =>
                     SCL   <= '0';
                     SDA01 <= DataIn(bitcount);
@@ -103,9 +119,13 @@ begin
                     DataOut <= DataIn;
                     state   <= SLV_ACK4;
                 when SLV_ACK4 =>
-                    SCL   <= '1';
-                    Ack   <= SDA;
-                    state <= STOP1;
+                    SCL <= '1';
+                    Ack <= SDA;
+                    if SDA = '1' then
+                        state <= ACK_ERROR;
+                    else
+                        state <= STOP1;
+                    end if;
                 -- Enviar stop condition
                 when STOP1 =>
                     SCL   <= '0';
@@ -119,10 +139,67 @@ begin
                     SCL   <= '1';
                     SDA01 <= '1';
                     state <= STOP3;
+                -- Recibir datos del esclavo
+                when RDDATA1 =>
+                    SCL   <= '0';
+                    SDA01 <= '1';
+                    state <= RDDATA2;
+                when RDDATA2 =>
+                    SCL            <= '1';
+                    Data(bitcount) <= SDA;
+                    if (bitcount - 1) >= 0 then
+                        bitcount <= bitcount - 1;
+                        state    <= RDDATA1;
+                    else
+                        bitcount <= 7;
+                        state    <= NACK1;
+                    end if;
+                -- Enviar NAck al esclavo
+                when NACK1 =>
+                    SCL     <= '0';
+                    SDA01   <= '1';
+                    state   <= NACK2;
+                    DataOut <= Data;
+                when NACK2 =>
+                    SCL   <= '1';
+                    state <= STOP4;
+                -- Enviar stop condition
+                when STOP4 =>
+                    SCL   <= '0';
+                    SDA01 <= '0';
+                    state <= STOP5;
+                when STOP5 =>
+                    SCL   <= '1';
+                    SDA01 <= '0';
+                    state <= STOP6;
+                when STOP6 =>
+                    SCL   <= '1';
+                    SDA01 <= '1';
+                    state <= STOP6;
+                -- Error, no se recibio Ack
+                when ACK_ERROR =>
+                    SCL   <= '1';
+                    SDA01 <= '1';
+                    state <= ACK_ERROR;
                 when OTHERS => null;
                     SCL   <= '1';
                     SDA01 <= '1';
             end case;
+        end if;
+    end process;
+
+    clockdivider200k : process
+    begin
+        wait until CLK_50_MHz'EVENT and CLK_50_MHz = '1';
+        if ClkCountDivider < C250 then
+            ClkCountDivider <= ClkCountDivider + 1;
+        else
+            ClkCountDivider <= 0;
+        end if;
+        if ClkCountDivider < C250/2 then
+            CLK_200k_Hz <= '0';
+        else
+            CLK_200k_Hz <= '1';
         end if;
     end process;
 end arch;
